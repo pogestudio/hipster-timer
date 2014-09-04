cbde6391e879595f3e57c63d6b3caf99dde4593d
CA Ewerbring <caewerbring@gmail.com>
2014-09-04 12:57:06 +0200

    adding xcode settings

--- timeleft/ViewController.m
+++ timeleft/ViewController.m
@@ -86,6 +86,9 @@ typedef enum {
 
 -(void)updateLabels
 {
+    if (!self.startDate || !self.endDate) {
+        return;
+    }
     NSDate *currentDate = [NSDate date];
     
 //    NSInteger currentHour = [self hoursFromDate:currentDate];
